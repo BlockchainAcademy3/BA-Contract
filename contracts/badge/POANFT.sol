38d967f645f0a5d1b35998ae600917d8521650c4
liyang <42539774+LIYANG-UST@users.noreply.github.com>
2022-09-22 00:19:56 +0800

    feat: add event id and course ids

--- contracts/badge/POANFT.sol
+++ contracts/badge/POANFT.sol
@@ -7,37 +7,31 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "../base/ERC721.sol";
 
-contract CourseNFT is Ownable, ERC721 {
+contract POANFT is ERC721, Ownable {
     using Strings for uint256;
 
     uint256 public counter;
 
-    // Token id => Course Id
-    // Course id is an index for the server to query the course
-    // After querying the correct course, the server can get the event data
-    mapping(uint256 => uint256) public courseIds;
-
-    // Expiry timestamp for a course id
-    // After this timestamp, the NFT is no longer available for mint
-    // If expiry == 0, the NFT is always available for mint
-    mapping(uint256 => uint256) public expiryForCourses;
-
     string public baseURI;
+
+    mapping(uint256 => uint256) public eventIds;
+
+    mapping(uint256 => uint256) public expiryForEvents;
 
     event BaseURIChanged(string oldURI, string newURI);
 
-    event CourseNFTMinted(
+    event POANFTMinted(
         address indexed to,
         uint256 indexed tokenId,
-        uint256 indexed courseId
+        uint256 indexed eventId
     );
 
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
     {}
 
-    modifier notExpiry(uint256 _courseId) {
-        uint256 expiryDate = expiryForCourses[_courseId];
+    modifier notExpiry(uint256 _eventId) {
+        uint256 expiryDate = expiryForEvents[_eventId];
         if (expiryDate != 0) {
             require(
                 block.timestamp < expiryDate,
@@ -52,9 +46,9 @@ contract CourseNFT is Ownable, ERC721 {
         baseURI = _uri;
     }
 
-    function mint(address _to, uint256 _courseId)
+    function mint(address _to, uint256 _eventId)
         public
-        notExpiry(_courseId)
+        notExpiry(_eventId)
         returns (uint256)
     {
         uint256 tokenId = counter++;
@@ -62,9 +56,9 @@ contract CourseNFT is Ownable, ERC721 {
         _safeMint(_to, tokenId);
 
         // Record this token's course Id
-        courseIds[tokenId] = _courseId;
+        eventIds[tokenId] = _eventId;
 
-        emit CourseNFTMinted(_to, tokenId, _courseId);
+        emit POANFTMinted(_to, tokenId, _eventId);
 
         return tokenId;
     }
@@ -80,6 +74,6 @@ contract CourseNFT is Ownable, ERC721 {
      *         URI = "https://api.blockchainacademy.org/api/v1/nft/course/{courseId}"
      */
     function _tokenURI(uint256 _tokenId) internal view returns (string memory) {
-        return string.concat(baseURI, courseIds[_tokenId].toString());
+        return string.concat(baseURI, eventIds[_tokenId].toString());
     }
 }
